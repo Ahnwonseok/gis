beb348dc7817934a0cc6c169be5f69a6907d2b3c
Ahnwonseok <anwonseok72@gmail.com>
2025-12-01 23:58:35 +0900

    1. cors 서버 추가 2. 백엔드 ip 변경

--- gis-be/src/main/java/project/gis/config/WebConfig.java
+++ gis-be/src/main/java/project/gis/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://gis-alpha.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
